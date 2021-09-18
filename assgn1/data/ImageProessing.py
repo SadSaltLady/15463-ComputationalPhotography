41d71a085e7975b6b2be8345befe4396a6b542c3
SadSaltLady <62451205+SadSaltLady@users.noreply.github.com>
2021-09-17 23:22:01 -0400

    all task completed, no documentation

--- assgn1/data/ImageProessing.py
+++ assgn1/data/ImageProessing.py
@@ -1,5 +1,7 @@
 import os
+import math
 from matplotlib.colors import Normalize
+from numpy.lib.shape_base import dstack
 from skimage import io
 import numpy as np
 from scipy import interpolate
@@ -46,6 +48,31 @@ def WBWhiteWorld(imR, imG, imB):
     maxR = np.amax(imR)
     maxG = np.amax(imG)
     maxB = np.amax(imB)
+    #apply transformation matrix(weight rbchannels)
+    WBimR = imR * (maxG/maxR)
+    WBimG = imG
+    WBimB = imB * (maxG/maxB)
+    #stack them here (or not)
+    #im_rgb = np.dstack((WBimR, WBimG, WBimB))
+    #but I guess the brightest spots are everywhere on the image, why would 
+    #this function make sense at all? 
+    return (WBimR, WBimG, WBimB)
+
+def WBWhiteWorldManual(imR, imG, imB):
+    #collected values manually and just putting them here as constants
+    #Currently using: Clouds
+    #White area: clouds
+
+    maxR = 0.431
+    maxG = 0.998
+    maxB = 0.69
+
+    #White area: stairs
+    '''
+    maxR = 0.312
+    maxG = 0.788
+    maxB = 0.527
+    '''
     #apply transformation matrix(weight rbchannels)
     WBimR = imR * (maxG/maxR)
     WBimG = imG
@@ -106,87 +133,105 @@ def PatternHelper(BayerType, init):
     imGreen = np.zeros([init.shape[0],init.shape[1]//2])
     imGreen[::2] = imG #first row
     imGreen[1::2] = imGG #second
-    return imR, imGreen, imB
+    return imR, imGreen, imB, imG, imGG
     #return WBGrayWorld(imR, imG, imB, imGG)
     
+#NOTE: demosaicing is assumping rggb layout
+def Demosaicing (imR, imGTop, imGBot, imB, width, height):
+    #Red channel
+    xRed = np.arange(0, width, 2)
+    yRed = np.arange(0, height, 2)
+    fRed = interpolate.interp2d(xRed, yRed, imR, kind='linear')
+    imRedChannel = fRed(range(width), range(height))
+    #Blue Channel
+    xBlue = np.arange(1, width, 2)
+    yBlue = np.arange(1, height, 2)
+    fBlue = interpolate.interp2d(xBlue, yBlue, imB, kind='linear')
+    imBlueChannel = fBlue(range(width), range(height))
+    #Top Green Chanel
+    xGTop = np.arange(0, width, 2)
+    yGTop = np.arange(1, height, 2)
+    fGTop = interpolate.interp2d(xGTop, yGTop, imGTop, kind = 'linear')
+    imGreenTop = fGTop(range(width), range(height))
+    #Bot Green Channel
+    xGBot = np.arange(1, width, 2)
+    yGBot = np.arange(0, height, 2)
+    fGBot = interpolate.interp2d(xGBot,yGBot, imGBot, kind = 'linear')
+    imGreenBot = fGBot(range(width), range(height))
+    #Combine and take average 
+    imGreenChannel = (imGreenTop + imGreenBot) / 2.
+    print(imGreenChannel)
+    return imRedChannel, imGreenChannel, imBlueChannel
 
+def GetInverseM():
+    mXYZCam = np.array(
+        [
+            [6988,-1384,-714],
+            [-5631,13410,2447],
+            [-1485,2204,7318]
+        ]
+    )*0.001
+
+    mRGBXYZ = np.array(
+        [
+            [0.4124564, 0.3575761, 0.1804375],
+            [0.2126729, 0.7151522, 0.0721750],
+            [0.0193339, 0.1191920, 0.9503041]
+        ]
+    )
+
+    mRGBCam = np.matmul(mXYZCam, mRGBXYZ)
+    #normalize it
+    sumOfRows = mRGBCam.sum(axis=1)
+    normalizedmRGBCam = mRGBCam / sumOfRows[:, np.newaxis]
+    return np.linalg.inv(normalizedmRGBCam)
+
+def GamEncoding(x):
+    if (x <= 0.0031308):
+        return 12.92 * x
+    else:
+        return ((1 + 0.055) * math.pow(x, 1./2.4)) - 0.055
+
+def linearScaling(scale, CSCRed, CSCGreen, CSCBlue):
+    gammaRed = CSCRed * scale
+    gammaGreen = CSCGreen * scale
+    gammaBlue = CSCBlue * scale
+    gammaRed = np.vectorize(GamEncoding)(gammaRed)
+    gammaBlue = np.vectorize(GamEncoding)(gammaBlue)
+    gammaGreen = np.vectorize(GamEncoding)(gammaGreen)
+    gammaRed = np.clip(gammaRed, 0.,1.)
+    gammaGreen = np.clip(gammaGreen, 0., 1.)
+    gammaBlue = np.clip(gammaBlue, 0. , 1.)
+    return (gammaRed, gammaGreen, gammaBlue)
 
 init = ProcessInitial()
-imRR, imGG, imBB = PatternHelper("rggb", init)
-imR, imG, imB = WBCameraScale(imRR, imGG, imBB)
+imRR, imGG, imBB, imGGTop, imGGBot = PatternHelper("rggb", init)
+imR, imG, imB = WBGrayWorld(imRR, imGG, imBB)
+
 #now interpolate the images
 print(init.shape)
 width = init.shape[1]
 height = init.shape[0]
-#Red channel
-xRed = np.arange(0, width, 2)
-yRed = np.arange(0, height, 2)
-fRed = interpolate.interp2d(xRed, yRed, imR, kind='linear')
-imRedChannel = fRed(range(width), range(height))
-print("red dimension")
-print(imRedChannel.shape)
-#Green Channel
-#added i%2 to account for odd number pixel images
-
-''' 
-########## this overflows and idk what to do with it lol
-xGreen = np.array([[j for j in range(i%2, width+i%2, 2)]for i in range(height)])
-print("xgreen")
-print(xGreen.shape)
-yGreen = np.array([[i for j in range(imG.shape[1])] for i in range(height)])
-print("ygreen")
-print(yGreen.shape)
-print("green")
-print(imG.shape)    
-fGreen = interpolate.interp2d(xGreen.flatten(), yGreen.flatten(), imG, kind='linear')
-imGreenChannel = fGreen(range(width), range(height))
-'''
-xGreen = np.arange(0, width, 2) #PROBLEM
-yGreen = np.arange(0, height)
-fGreen = interpolate.interp2d(xGreen, yGreen, imG, kind='linear')
-imGreenChannel = fGreen(range(width), range(height))
-#Blue Channel
-xBlue = np.arange(1, width, 2)
-yBlue = np.arange(1, height, 2)
-print(len(yBlue))
-fBlue = interpolate.interp2d(xBlue, yBlue, imB, kind='linear')
-imBlueChannel = fBlue(range(width), range(height))
-
-
-
-mXYZCam = np.array(
-    [
-        [6988,-1384,-714],
-        [-5631,13410,2447],
-        [-1485,2204,7318]
-    ]
-)*0.001
-
-mRGBXYZ = np.array(
-    [
-        [0.4124564, 0.3575761, 0.1804375],
-        [0.2126729, 0.7151522, 0.0721750],
-        [0.0193339, 0.1191920, 0.9503041]
-    ]
-)
-
-mRGBCam = np.matmul(mXYZCam, mRGBXYZ)
-#normalize it
-sumOfRows = mRGBCam.sum(axis=1)
-normalizedmRGBCam = mRGBCam / sumOfRows[:, np.newaxis]
-inverseM = np.linalg.inv(normalizedmRGBCam)
-
+imRedChannel, imGreenChannel, imBlueChannel = Demosaicing (imR, imGGTop, imGGBot, imB, width, height)
+inverseM = GetInverseM()
 rgb_camera = np.dstack((imRedChannel, imGreenChannel, imBlueChannel))
+#can I still keep my channels separatae at this point - yes
+CSCRed = imRedChannel * inverseM[0][0] + imGreenChannel * inverseM[0][1] + imBlueChannel*inverseM[0][2]
+CSCGreen = imRedChannel * inverseM[1][0] + imGreenChannel * inverseM[1][1] + imBlueChannel*inverseM[1][2]
+CSCBlue = imRedChannel * inverseM[2][0] + imGreenChannel * inverseM[2][1] + imBlueChannel*inverseM[2][2]
+
+#Brightness adjustment and gamma encoding 
+LinearScalingConst = 1.55
+gammaRed, gammaGreen, gammaBlue = linearScaling(LinearScalingConst, CSCRed, CSCGreen, CSCBlue)
+rgb_camera = np.dstack((gammaRed, gammaGreen, gammaBlue))
+
+
 plt.imshow(rgb_camera)
 plt.show()
+#save the picture
+#io.imsave('compiled-25.jpg', rgb_camera, quality = 25)
 
 
-def mult(m):
-    return np.matmul(inverseM,m)
-why = np.apply_along_axis(mult,2,rgb_camera)
-print(why.shape)
-plt.imshow(why)
-plt.show()
 '''
 rgb_test = np.dstack((imRedChannel, imGreenChannel, imBlueChannel))
 print(np.shape(rgb_test))
